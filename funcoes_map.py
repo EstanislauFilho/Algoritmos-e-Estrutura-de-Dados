47eab0f4167ee165f6e0cf6931574654cca7ecd1
EstanislauFilho <estanislau.sena@gmail.com>
2022-04-16 12:22:53 -0300

    Funções map.

---
+++ funcoes_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 12:20:49 2022
+
+@author: estanislau
+"""
+
+lista = [1, 2, 3]
+
+m = map(lambda x: x ** 2, lista)
+
+for i in m:
+    print(i)
