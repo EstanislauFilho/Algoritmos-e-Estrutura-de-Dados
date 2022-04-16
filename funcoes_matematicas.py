91c4b2a47b542fbfdfcfedeb34a32d75b8a12141
EstanislauFilho <estanislau.sena@gmail.com>
2022-04-16 13:03:19 -0300

    Funções matematicas.

--- funcoes_matematicas.py
+++ funcoes_matematicas.py
@@ -11,8 +11,16 @@ def fatorial(n):
         return 1
     return (n * fatorial(n-1))
 
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+
 fatorial_lambda = lambda n: n * fatorial(n-1) if n > 1 else 1
 
 
 print(fatorial(5))
 print(fatorial_lambda(5))
+print(fibonacci(6))
