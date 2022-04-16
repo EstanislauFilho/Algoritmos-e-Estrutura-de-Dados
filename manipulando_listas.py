4f9ca663809313b63db34a05628ec1d04cf3414e
EstanislauFilho <estanislau.sena@gmail.com>
2022-04-16 13:27:21 -0300

    Manipulando listas.

---
+++ manipulando_listas.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 13:23:34 2022
+
+@author: estanislau
+"""
+
+lista = [1, 10, 16, 2, 9, 2, 3, 4, 5]
+
+lista.insert(5, 13)
+print(lista)
+
+lista.remove(2)
+print(lista)
+
+lista.append(6)
+print(lista)
+
+lista.pop(7)
+print(lista)
+
+print(lista[-1])
