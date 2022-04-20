828009b05ad9ba4eb3df0c629ab5a01aa45ed024
EstanislauFilho <estanislau.sena@gmail.com>
2022-04-20 20:43:26 -0300

    Estrutura de dados Pilha Otimizado.

--- pilha.py
+++ pilha.py
@@ -5,36 +5,53 @@ Created on Wed Apr 20 20:06:30 2022
 
 @author: estanislau
 """
-
+import time
 class Pilha:
     
     def __init__(self):
         self.pilha = []
+        self.contador = 0
         
     def inserir_na_pilha(self, valor):
         """
         Insere valor sempre na ultima posição da lista
         """
         self.pilha.append(valor)
+        self.contador += 1
         
     def remover_na_pilha(self):
         """
         Remove no topo da pilha somente se a mesma não estiver vazia
         """
-        if len(self.pilha) > 0:
-            self.pilha.pop(len(self.pilha)-1)
-
+        if self.contador > 0:
+            self.pilha.pop(-1)
+            self.contador -= 1
   
           
 if __name__ == '__main__':
+    t_ini = time.time()
     p = Pilha()
     
     p.remover_na_pilha()
     
+    p.inserir_na_pilha(1)
+    p.inserir_na_pilha(2)
+    p.inserir_na_pilha(3)
+    p.inserir_na_pilha(4)
     p.inserir_na_pilha(5)
+    p.inserir_na_pilha(6)
+    
+    p.remover_na_pilha()
+    
+    p.inserir_na_pilha(6)
+    p.inserir_na_pilha(7)
+    p.inserir_na_pilha(8)
     p.inserir_na_pilha(9)
-    p.inserir_na_pilha(34)
+    p.inserir_na_pilha(9)
     
     p.remover_na_pilha()
     
     print(p.pilha)
+    
+    t_fim = time.time()
+    print(f'Tempo de execução: {(t_fim - t_ini):.5f} seg')
