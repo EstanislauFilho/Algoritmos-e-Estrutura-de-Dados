c8fecb7940471958945071694090937b22b870fd
EstanislauFilho <estanislau.sena@gmail.com>
2022-06-13 21:08:26 -0300

    Estrutura de dados: Algoritmo de busca binária.

---
+++ busca_binaria.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 20:40:04 2022
+
+@author: estanislau
+"""
+
+def busca_binaria(lista, chave, ini, fim):
+    if ini > fim:
+        return False
+    
+    meio = (ini + fim)//2
+    
+    if chave == lista[meio]:
+        return True
+    if chave < lista[meio]:
+        return busca_binaria(lista, chave, ini, meio - 1)
+    return busca_binaria(lista, chave, meio + 1, fim)
+
+
+lista = [11, 5, 10, 20, 15, 4]
+chave = 26
+lista.sort()
+
+print(busca_binaria(lista, chave, 0, len(lista) - 1))
