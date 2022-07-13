7d0f6f9153616be2faa6f1edb3e456fb32456858
EstanislauFilho <estanislau.sena@gmail.com>
2022-07-13 20:47:05 -0300

    Testando novo calculo de tempo de execução.

--- busca_binaria.py
+++ busca_binaria.py
@@ -5,6 +5,7 @@ Created on Mon Jun 13 20:40:04 2022
 
 @author: estanislau
 """
+import datetime
 
 def busca_binaria(lista, chave, ini, fim):
     if ini > fim:
@@ -19,8 +20,14 @@ def busca_binaria(lista, chave, ini, fim):
     return busca_binaria(lista, chave, meio + 1, fim)
 
 
+inicio = datetime.datetime.now()
+print(inicio)
 lista = [11, 5, 10, 20, 15, 4]
-chave = 26
+chave = 11
 lista.sort()
 
 print(busca_binaria(lista, chave, 0, len(lista) - 1))
+
+tempo = datetime.datetime.now() - inicio
+
+print(f"Terminou em {tempo.total_seconds():.8f} segundos.")
